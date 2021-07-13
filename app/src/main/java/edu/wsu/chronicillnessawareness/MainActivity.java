9aab62edd37e98ca06eb1073a0b9045e635018db
Reem Tamimi <rntamimi@gmail.com>
2021-07-12 22:21:23 -0400

    functional buttons on home page and option menu

--- app/src/main/java/edu/wsu/chronicillnessawareness/MainActivity.java
+++ app/src/main/java/edu/wsu/chronicillnessawareness/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
+
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -23,8 +28,25 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+
     }
 
+    public void openJoinGroup(){
+        Intent intent = new Intent(this, JoinGroup.class);
+        startActivity(intent);
+    }
+
+    public void openCreateGroup(){
+        Intent intent = new Intent(this, CreateGroup.class);
+        startActivity(intent);
+    }
+
+    public void openShareExperience(){
+        Intent intent = new Intent(this, ShareExperience.class);
+        startActivity(intent);
+    }
+
+    // creating navigation bar for home, notifications, and profile
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
