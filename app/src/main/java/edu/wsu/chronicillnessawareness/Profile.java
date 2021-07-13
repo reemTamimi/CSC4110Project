4e9e4ee2bec017d6c1310f76e1b8aeb9aebde1b6
Rubina Akther <ga6899@wayne.edu>
2021-07-13 16:51:31 -0400

    Forget Password

--- app/src/main/java/edu/wsu/chronicillnessawareness/Profile.java
+++ app/src/main/java/edu/wsu/chronicillnessawareness/Profile.java
@@ -9,6 +9,6 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_user_profile);
     }
 }
