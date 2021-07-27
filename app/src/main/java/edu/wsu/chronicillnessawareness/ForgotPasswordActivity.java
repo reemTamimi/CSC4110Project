a27819905a48f4414832e721f92c8cc7f58b67a3
Rubina Akther <ga6899@wayne.edu>
2021-07-26 22:17:19 -0400

    Forget Password

--- app/src/main/java/edu/wsu/chronicillnessawareness/ForgotPasswordActivity.java
+++ app/src/main/java/edu/wsu/chronicillnessawareness/ForgotPasswordActivity.java
@@ -2,13 +2,18 @@ package edu.wsu.chronicillnessawareness;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    TextView signIn,signUp;
+    private EditText mEmail;
 
+    Button button;
+    ProgressBar progressBar4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
