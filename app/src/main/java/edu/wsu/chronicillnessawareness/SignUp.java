c79684f3ce29459efb4cab3bbb985c42f0596142
Rubina Akther <ga6899@wayne.edu>
2021-07-27 09:58:03 -0400

    Forget Password

--- app/src/main/java/edu/wsu/chronicillnessawareness/SignUp.java
+++ app/src/main/java/edu/wsu/chronicillnessawareness/SignUp.java
@@ -1,8 +1,30 @@
 package edu.wsu.chronicillnessawareness;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import edu.wsu.chronicillnessawareness.menu.Policy;
+import edu.wsu.chronicillnessawareness.R;
+import com.tapadoo.alerter.Alerter;
+import java.util.HashMap;
+import java.util.Objects;
+
+
 
 public class SignUp extends AppCompatActivity {
 
