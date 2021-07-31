9edc82ef86a042a170fa6a59c57d8e29736e5ba5
Reem Tamimi <rntamimi@gmail.com>
2021-07-30 20:33:26 -0400

    updated changes

---
+++ app/src/main/java/edu/wsu/chronicillnessawareness/Model/ModelChatlist.java
@@ -0,0 +1,22 @@
+package edu.wsu.chronicillnessawareness.Model;
+
+@SuppressWarnings({"ALL", "unused"})
+public class ModelChatlist {
+    String id;
+
+    public ModelChatlist(String id) {
+        this.id = id;
+    }
+
+    @SuppressWarnings("unused")
+    public ModelChatlist() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
